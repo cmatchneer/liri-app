cd8833e315c99d4245c25c6c8d12e5456fca5cb5
cmatchneer <cmatchneer@yahoo.com>
2020-01-06 18:00:49 -0500

    basics all work

--- keys.js
+++ keys.js
@@ -1,5 +1,4 @@
-console.log("loaded");
 exports.spotify = {
-    id: "d166d6d5ebcd4ecfa87e68c2f5937d10",
-    secret: "a293c0757fa24c769e210ac6f666028c"
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 };
